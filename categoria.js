945a9e3d675d2784851dd83cfba5ea610029984e
EZ-Consumibles <refaccionesez@gmail.com>
2026-07-01 12:51:11 -0600

    Update categoria.js

--- categoria.js
+++ categoria.js
@@ -1,3 +1,4 @@
+alert("categoria.js funcionando");
 import { db } from "./firebase.js";
 import { collection, getDocs} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 // Obtener categoría desde la URL
